0f03e7653efc5842db4ea1768b9ddc0c4d9115af
Jiří Staniševský <jirist@salsitasoft.com>
2022-11-10 09:15:04 +0100

    feat(bin)!: Remove release supporting tools (#44)
    * feat(bin)!: Remove release supporting tools
    
    - lerna is now used for the release process including changelogs
    
    * docs: Cleanup outdated docs
    
    - remove docs for the removed bin commands
    - update info on PRs and releasing
    
    Co-authored-by: Jiri Vejmola <jiri@salsitasoft.com>

--- packages/bin/src/internal/utils/git.ts
+++
@@ -1,6 +0,0 @@
-import spawnAsync from '@expo/spawn-async'
-
-export async function getCurrentBranchName () {
-  const { stdout } = await spawnAsync('git', ['rev-parse', '--abbrev-ref', 'HEAD'])
-  return stdout.trim()
-}
